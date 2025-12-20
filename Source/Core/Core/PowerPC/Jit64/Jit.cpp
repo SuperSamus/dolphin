a30a697ece9c2f3baf01ca4218ddb0047796edcf
Martino Fontana <tinozzo123@gmail.com>
2025-12-20 18:46:25 +0100

    Jit64: Some RegCache documentation improvements. Add some comments, and prefer using ASSERTs in the callees instead of the callers.

--- Source/Core/Core/PowerPC/Jit64/Jit.cpp
+++ Source/Core/Core/PowerPC/Jit64/Jit.cpp
@@ -1214,8 +1214,8 @@ bool Jit64::DoJit(u32 em_address, JitBlock* b, u32 nextPC)
         RCForkGuard gpr_guard = gpr.Fork();
         RCForkGuard fpr_guard = fpr.Fork();
 
-        gpr.Revert();
-        fpr.Revert();
+        gpr.RevertStaged();
+        fpr.RevertStaged();
         gpr.Flush();
         fpr.Flush();
 
@@ -1224,8 +1224,8 @@ bool Jit64::DoJit(u32 em_address, JitBlock* b, u32 nextPC)
         SwitchToNearCode();
       }
 
-      gpr.Commit();
-      fpr.Commit();
+      gpr.CommitStaged();
+      fpr.CommitStaged();
 
       // If we have a register that will never be used again, discard or flush it.
       if (!bJITRegisterCacheOff)
