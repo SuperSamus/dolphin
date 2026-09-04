f0eb5d60b50d01f0609cb26a81136dc6d52beef7
Martino Fontana <tinozzo123@gmail.com>
2026-09-05 00:23:14 +0200

    Jit64: Merge m_exception_handler_at_loc in TrampolineInfo
    If MMU is enabled, there's an m_exception_handler_at_loc entry for almost every entry in m_back_patch_info, so keeping them separate is a huge waste: hashmaps aren't free.
    Brings a decent improvement to codegen performance when MMU is enabled.
    
    If MMU is disabled, however, this increases the size of TrampolineInfo by 8 bytes. It's a little sad, but it's important to prioritize the worst-case performance.
    Also, it simplifies the next commit, which will reduce the memory usage anyway.

--- Source/Core/Core/PowerPC/Jit64Common/TrampolineInfo.h
+++ Source/Core/Core/PowerPC/Jit64Common/TrampolineInfo.h
@@ -8,7 +8,7 @@
 #include "Common/x64Emitter.h"
 
 // Stores information we need to batch-patch a MOV with a call to the slow read/write path after
-// it faults. There will be 10s of thousands of these structs live, so be wary of making this too
+// it faults. There will be 100s of thousands of these structs live, so be wary of making this too
 // big.
 struct TrampolineInfo final
 {
@@ -46,4 +46,7 @@ struct TrampolineInfo final
   s32 offset = 0;
   Gen::X64Reg op_reg{};
   Gen::OpArg op_arg{};
+
+  // It's not set if memcheck is disabled. Otherwise, it's almost always set.
+  u8* exception_handler_at_loc = nullptr;
 };
